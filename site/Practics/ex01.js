7a2d2d4aebb57a14320a70c2939beb6d79ee0117
Cristian Valdivieso <cristian.valdivieso@unesp.br>
2020-07-17 15:56:14 -0300

    Create ex01.js

---
+++ site/Practics/ex01.js
@@ -0,0 +1,36 @@
+/* var myOb = {
+    nombre: "Cristian",
+    curso: "HTML/CSS/JS",
+    plataforma: "Coursera"
+};
+for (var prop in myOb) {
+    console.log(prop + ": " + myOb[prop]);
+} */
+
+
+
+
+/* var names2 = ["Cris", "Joyce", "Pilar"];
+
+for (var name in names2) {
+    console.log("Hola, " + names2[name])
+} */
+
+
+function makeMultiplier (multiplier){
+    // var multiplier = 2
+
+    function b() {
+        console.log("Multiplier is: " + multiplier);
+    }
+    b();
+
+
+    return (
+        function (x) {
+            return multiplier * x
+        }
+    );
+}
+var doubleAll = makeMultiplier(2);
+console.log(doubleAll(100))
